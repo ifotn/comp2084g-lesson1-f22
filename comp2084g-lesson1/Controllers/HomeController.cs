43602ec906937c62834e2144c32e7532aac570dc
ifotn <rich@infrontofthenet.com>
2022-09-08 10:24:17 -0400

    Add project files.

---
+++ comp2084g-lesson1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using comp2084g_lesson1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace comp2084g_lesson1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This comes from the controller.  It's currently " + DateTime.Now.ToString();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ comp2084g-lesson1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace comp2084g_lesson1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
